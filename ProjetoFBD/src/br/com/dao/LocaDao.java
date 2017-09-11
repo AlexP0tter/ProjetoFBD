d4ca85bd48661cd386f93c917a941c9bbcc01e34
Jesitto <Jesitto@Jesitto-PC>
2017-09-10 21:49:31 -0300

    Merge origin/master
    Conflicts:
    	ProjetoFBD/src/Frame/EscolhaCarro.java

--- ProjetoFBD/src/br/com/dao/LocaDao.java
+++ ProjetoFBD/src/br/com/dao/LocaDao.java
@@ -65,8 +65,7 @@ public class LocaDao {
     public LocacaoModel alterar(LocacaoModel loc)throws Exception {
         try {
             statement = con.prepareStatement(SqlUtil.UPDATE_LOCACAO);            
-            
-                        
+          
             statement.setString(1, loc.getDataRetirada());
             statement.setString(2, loc.getDataDevolucao());
             statement.setDouble(3, loc.getValor());
